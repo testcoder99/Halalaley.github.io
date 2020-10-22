03624bd5b48f809a22a234e60f592358797ff87c
Halalaley <69675994+Halalaley@users.noreply.github.com>
2020-10-22 12:54:30 +0600

    viking

---
+++ viking/js/main.js
@@ -0,0 +1,29 @@
+$(function(){
+  $('.menu__btn').on('click',function(){
+    $('.menu__list').toggleClass('menu__list--active')
+  });
+
+
+ $('.heroes__slider-img').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  asNavFor: '.heroes__slider-text',
+  prevArrow:'<button  class="slick-btn slick-prev"><img src="images/arrow-prev.png" alt=""></button>',
+  nextArrow:'<button  class="slick-btn slick-next"><img src="images/arrow-next.png" alt=""></button>',
+  responsive:[
+  {
+    breakpoint: 769,
+    settings:{
+      arrows:false,
+    }
+  }
+]
+});
+$('.heroes__slider-text').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  asNavFor: '.heroes__slider-img',
+  fade: true,
+  arrows:false,
+});
+});
