c46bf6e74b24c94dc0019fff2dd5cf8af7d2b5a7
Halalaley <69675994+Halalaley@users.noreply.github.com>
2021-02-23 18:54:29 +0500

    op

--- Theatre/js/main.js
+++ Theatre/js/main.js
@@ -25,7 +25,7 @@ $(function(){
 	$('.close-btn').on('click', function(){
 		$('.header__mobile').removeClass('active');
 	});
-	  	$('.performances__body').slick('setPosition');
+
 	if ( $(window).width() < 1367 ) {
 
 		$('.performances__body').slick({
