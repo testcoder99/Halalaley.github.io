bd5c3d86e2cbb9389273d5e2a9047c69ee6ee042
Halalaley <69675994+Halalaley@users.noreply.github.com>
2021-03-24 11:46:15 +0500

    update

--- Business/js/main.js
+++ Business/js/main.js
@@ -1,9 +1,7 @@
-$(function(){
-	document.querySelector('.menu__btn').addEventListener('click', function(){
-		this.classList.toggle('menu__btn--active');
-		document.querySelector('.header__top-mobile').classList.toggle('header__top-mobile--active');
-	})
-})	
+document.querySelector('.menu__btn').addEventListener('click', function(){
+	this.classList.toggle('menu__btn--active');
+	document.querySelector('.header__top-mobile').classList.toggle('header__top-mobile--active');
+})
 
 
 
