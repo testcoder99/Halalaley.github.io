5bab2464d24ac6314f0f20a61f3a6d0d9788f955
Halalaley <69675994+Halalaley@users.noreply.github.com>
2021-03-26 22:12:05 +0500

    update

--- Design/js/main.js
+++ Design/js/main.js
@@ -3,7 +3,50 @@ $(function(){
 		arrows:false,
 		autoplay:true,
 	})
+	
+	var header =$(".header"),
+		headerTop =$("#header__top"),
+		introB = $("#body").innerHeight(),
+		scrollOfset = $(window).scrollTop();
 
+    checkScroll(scrollOfset);		
+
+	$(window).on("scroll", function() {
+		
+		scrollOfset = $(this).scrollTop();
+
+		checkScroll(scrollOfset);
+		
+	});
+
+	function checkScroll(scrollOfset) {
+
+		if( scrollOfset >= introB) {
+			header.addClass("header__active-fixed");
+			headerTop.addClass("header__top-fixed");
+		}
+		else{
+			header.removeClass("header__active-fixed");
+			headerTop.removeClass("header__top-fixed");
+		}
+	};
+
+	$("[data-scroll").on("click", function(event){
+		event.preventDefault();
+
+		var	$this = $(this), 
+			blockId = $this.data("scroll"),
+			blockOffset = $(blockId).offset().top;
+		$(".menu-mobile").removeClass("menu-mobile--active");
+		// $("nav__toggle").removeClass("active")
+		$(".header__top-link").removeClass("active")	
+		$this.addClass("active");
+
+		$("html, body").animate({
+			scrollTop: blockOffset
+		}, 1500);
+		
+	});
 });
 
 
@@ -48,6 +91,4 @@ if (animItems.length > 0) {
 	setTimeout(() => {
 		animOnScroll();
 	}, 300);
-
-	
 }
